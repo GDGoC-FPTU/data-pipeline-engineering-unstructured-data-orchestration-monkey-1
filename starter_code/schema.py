a38f8b36597a38234bb0d52cc781ea6f6df07051
Alblue123 <newborniphone@gmail.com>
2026-04-24 10:51:22 +0700

    feat: add schema

--- starter_code/schema.py
+++ starter_code/schema.py
@@ -9,5 +9,9 @@ class UnifiedDocument(BaseModel):
     Hệ thống cần 6 trường thông tin chuẩn (document_id, source_type, author, category, content, timestamp). 
     TODO: Khai báo các trường với kiểu dữ liệu str ở dưới.
     """
-    # Khai báo các trường ở đây...
-    pass
+    document_id: str = Field(..., description="ID duy nhất của tài liệu")
+    source_type: str = Field(..., description="Nguồn dữ liệu: PDF hoặc Video")
+    author: str = Field(..., description="Tác giả hoặc người tạo nội dung")
+    category: str = Field(..., description="Danh mục nội dung")
+    content: str = Field(..., description="Nội dung văn bản đã được làm sạch")
+    timestamp: str = Field(..., description="Thời gian tạo hoặc phát hành")
